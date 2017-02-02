dc7ae6683a348529124f501dd9c3fbe993800849
Marco Bassaletti <bassaletti@gmail.com>
2017-02-02 00:31:32 -0300

    Make App a MobX observer.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {observer} from 'mobx-react';
 import './App.css';
 import MenuBar from './MenuBar';
 import KanbanBoard from './KanbanBoard';
@@ -7,53 +8,55 @@ import Task from './Task';
 import AboutModal from './AboutModal';
 import MenuItemKey from './MenuItemKey';
 
-class App extends Component {
+const App = observer(
+    class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            showAboutModal: false,
-            taskLists: [
-                new TaskList('BackLog', [
-                    new Task('Add decorator support', 'This is needed to improve readability'),
-                    new Task('Merge with some themeforest template', 'Default bootstrap theme is ugly')
-                ]),
-                new TaskList('Doing', [
-                    new Task('Create backend', 'Try node, it\'s fast and light on server-side'),
-                    new Task('Integrate OAuth', 'Users should use their Google/Whatever accounts')
-                ]),
-                new TaskList('Done', [
-                    new Task('Create Git Repo', 'We need undo support for project')
-                ])
-            ]
-        };
-        this.handleMenuItemSelected = this.handleMenuItemSelected.bind(this);
-        this.closeAboutModal = this.closeAboutModal.bind(this);
-    }
+        constructor(props) {
+            super(props);
+            this.state = {
+                showAboutModal: false,
+                taskLists: [
+                    new TaskList('BackLog', [
+                        new Task('Add decorator support', 'This is needed to improve readability'),
+                        new Task('Merge with some themeforest template', 'Default bootstrap theme is ugly')
+                    ]),
+                    new TaskList('Doing', [
+                        new Task('Create backend', 'Try node, it\'s fast and light on server-side'),
+                        new Task('Integrate OAuth', 'Users should use their Google/Whatever accounts')
+                    ]),
+                    new TaskList('Done', [
+                        new Task('Create Git Repo', 'We need undo support for project')
+                    ])
+                ]
+            };
+            this.handleMenuItemSelected = this.handleMenuItemSelected.bind(this);
+            this.closeAboutModal = this.closeAboutModal.bind(this);
+        }
 
-    handleMenuItemSelected(menuItemKey) {
-        switch (menuItemKey) {
-            case MenuItemKey.ABOUT:
-                this.setState({ showAboutModal: true });
-                break;
+        handleMenuItemSelected(menuItemKey) {
+            switch (menuItemKey) {
+                case MenuItemKey.ABOUT:
+                    this.setState({showAboutModal: true});
+                    break;
+            }
+        }
+
+        closeAboutModal() {
+            this.setState({
+                showAboutModal: false
+            });
+        }
+
+        render() {
+            return (
+                <div className="App">
+                    <MenuBar onMenuItemSelected={this.handleMenuItemSelected}/>
+                    <KanbanBoard taskLists={this.state.taskLists}/>
+                    <AboutModal show={this.state.showAboutModal} onClose={this.closeAboutModal}/>
+                </div>
+            );
         }
     }
-
-    closeAboutModal() {
-        this.setState({
-            showAboutModal: false
-        });
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <MenuBar onMenuItemSelected={this.handleMenuItemSelected} />
-                <KanbanBoard taskLists={this.state.taskLists} />
-                <AboutModal show={this.state.showAboutModal} onClose={this.closeAboutModal} />
-            </div>
-        );
-    }
-}
+);
 
 export default App;
